f5f993f35ffb50806f973299afabf9e833dc42bb
Yukun He <yukunhe@berkeley.edu>
2015-03-15 19:39:29 -0700

    hw5 done

--- assignments/hw5/bml_simulation.R
+++ assignments/hw5/bml_simulation.R
@@ -7,4 +7,43 @@
 #### The output can e.g. be how many steps the system took until it hit gridlock or
 #### how many steps you observered before concluding that it is in a free flowing state.
 
+source("bml_functions.R")
 
+bml.sim(100,100,0.3)
+bml.sim(100,100,0.3)
+bml.sim(100,100,0.3)
+bml.sim(100,100,0.3)
+bml.sim(100,100,0.3)
+
+bml.sim(100,100,0.4)
+bml.sim(100,100,0.4)
+bml.sim(100,100,0.4)
+bml.sim(100,100,0.4)
+bml.sim(100,100,0.4)
+
+bml.sim(100,100,0.6)
+bml.sim(100,100,0.6)
+bml.sim(100,100,0.6)
+bml.sim(100,100,0.6)
+bml.sim(100,100,0.6)
+
+bml.sim(100,100,0.8)
+bml.sim(100,100,0.8)
+bml.sim(100,100,0.8)
+bml.sim(100,100,0.8)
+bml.sim(100,100,0.8)
+
+bml.sim(10,10,0.4)
+bml.sim(10,10,0.4)
+bml.sim(10,10,0.4)
+bml.sim(10,10,0.4)
+bml.sim(10,10.0.4)
+
+bml.sim(10,1000,0.4)
+bml.sim(10,1000,0.4)
+bml.sim(10,1000,0.4)
+bml.sim(10,1000,0.4)
+bml.sim(10,1000,0.4)
+
+s <- data.frame("0.3"=c(1000,1000,1000,1000,1000),"0.4"=c(828,599,630,1000,1000),"0.6"=c(195,362,375,282,349),"0.8"=c(93,78,47,62,64))
+boxplot(s, xlab="probability",ylab="steps to gridlock")
